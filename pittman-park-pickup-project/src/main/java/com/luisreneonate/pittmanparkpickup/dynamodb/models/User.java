41b0832f5b3d233bc7c464603f4ddf33d48df8fa
Rene Onate <lonate12@gmail.com>
2023-09-06 12:02:13 -0400

    Added initial User model

--- pittman-park-pickup-project/src/main/java/com/luisreneonate/pittmanparkpickup/dynamodb/models/User.java
+++ pittman-park-pickup-project/src/main/java/com/luisreneonate/pittmanparkpickup/dynamodb/models/User.java
@@ -1,4 +1,89 @@
 package main.java.com.luisreneonate.pittmanparkpickup.dynamodb.models;
 
+import java.util.Objects;
+import java.util.UUID;
+
+@DynamoDBTable(tableName = "user")
 public class User {
+    private UUID userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String phoneNumber) {
+        this.userId = UUID.randomUUID();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.role = this.email.equals("lonate12@gmail.com") ? "admin" : "player";
+    }
+
+    @DynamoDBHashKey(attributeName = "userId")
+    public UUID getUserId() {
+        return userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @DynamoDBAttribute(attributeName = "lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @DynamoDBAttribute(attributeName = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @DynamoDBAttribute(attributeName = "phoneNumber")
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @DynamoDBAttribute(attributeName = "role")
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
 }
