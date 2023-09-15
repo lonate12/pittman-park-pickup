0dd987d958dc0dfb47bfcde80a1b25d1fec0267e
Rene Onate <lonate12@gmail.com>
2023-09-15 14:56:07 -0400

    Filled in CreateUserRequest and CreateUserResult objects.

--- pittman-park-pickup-project/src/main/java/com/luisreneonate/pittmanparkpickup/apimodels/requests/CreateUserRequest.java
+++ pittman-park-pickup-project/src/main/java/com/luisreneonate/pittmanparkpickup/apimodels/requests/CreateUserRequest.java
@@ -1,6 +1,64 @@
 package main.java.com.luisreneonate.pittmanparkpickup.apimodels.requests;
 
+import java.util.Objects;
+
 public class CreateUserRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+
     public CreateUserRequest() {
+    }
+
+    public CreateUserRequest(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CreateUserRequest)) return false;
+        CreateUserRequest that = (CreateUserRequest) o;
+        return Objects.equals(getEmail(), that.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEmail());
+    }
+
+    @Override
+    public String toString() {
+        return "CreateUserRequest{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
--- pittman-park-pickup-project/src/main/java/com/luisreneonate/pittmanparkpickup/apimodels/results/CreateUserResult.java
+++ pittman-park-pickup-project/src/main/java/com/luisreneonate/pittmanparkpickup/apimodels/results/CreateUserResult.java
@@ -1,6 +1,34 @@
 package main.java.com.luisreneonate.pittmanparkpickup.apimodels.results;
 
+import main.java.com.luisreneonate.pittmanparkpickup.apimodels.UserModel;
+
 public class CreateUserResult {
-    public CreateUserResult() {
+    private UserModel user;
+
+    public CreateUserResult(Builder builder) {
+        this.user = builder.user;
+    }
+
+    public UserModel getUser() {return user;}
+
+    public void setUser(UserModel user) {
+        this.user = user;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private UserModel user;
+
+        public Builder withUser(UserModel userToUse) {
+            this.user = userToUse;
+            return this;
+        }
+
+        public CreateUserResult build() {
+            return new CreateUserResult(this);
+        }
     }
 }
